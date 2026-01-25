e016e9ef41d398260a67e2d9aa02b9320e44d3af
Gaganrobin8831 <gaganrobin71@gmail.com>
2026-01-25 18:52:49 +0530

    Update Events.jsx
    set the width of code

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -27,7 +27,7 @@ const Events = () => {
         <h1 className="text-center text-2xl sm:text-3xl lg:text-[35px] font-extrabold text-orange-400 mb-6 about-title">
         School activities
       </h1>
-      <p className='w-[60%] mx-auto p-7 text-center'>School activities play a crucial role in shaping students academic, social, and personal development. These activities go beyond the standard classroom curriculum and provide students with practical learning experiences.</p>
+      <p className='lg:w-[60%] mx-auto p-7 text-center'>School activities play a crucial role in shaping students academic, social, and personal development. These activities go beyond the standard classroom curriculum and provide students with practical learning experiences.</p>
       <div className='min-h-[80%] flex flex-wrap justify-center gap-4'>
         {
            imgArr.map((value,index)=>(
