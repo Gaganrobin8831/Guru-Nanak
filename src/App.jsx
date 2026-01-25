e8d934930bd6de7a8e46efb0a81335ab76eed71b
Gaganrobin8831 <gaganrobin71@gmail.com>
2026-01-25 18:47:06 +0530

    Update App.jsx
    for scroll to top after change router

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import {Routes,Route} from 'react-router-dom'
 import Hero from './pages/Hero'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
+import ScrollToTop from "./components/ScrollToTop";
 import Events from './pages/Events'
 const App = () => {
   return (
     <div className="bg-[url('/school.jpg')] bg-cover bg-center min-h-screen pt-20">
     <Nav/>
+<ScrollToTop />
     <Routes>
         <Route path='/' element={<Hero/>}/>
         <Route path='/events' element={<Events/>}/>
