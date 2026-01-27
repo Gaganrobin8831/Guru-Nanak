626ee43701ee6189917b474395e895324afb1f32
Gaganrobin8831 <gaganrobin71@gmail.com>
2026-01-27 19:13:44 +0530

    Update slider.jsx
    change some code snippet

--- src/pages/slider.jsx
+++ src/pages/slider.jsx
@@ -1,18 +1,11 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-
-
-// import required modules
-import { EffectFade, Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { EffectFade, Pagination, Autoplay } from 'swiper/modules';
 
 const Slider = () => {
-
   const imgArr = [
     "/slider/slide4.jpg",
     "/slider/slide13.jpg",
@@ -22,46 +15,41 @@ const Slider = () => {
     "/slider/slide15.jpg",
     "/slider/slide16.jpg",
     "/slider/slide3.jpg",
-
     "/slider/slide1.jpg",
     "/slider/slide5.jpg",
     "/slider/slide6.jpg",
     "/slider/slide7.jpg",
-    // "/slider/slide8.jpg",
     "/slider/slide9.jpg",
-    // "/slider/slide10.jpg",
     "/slider/slide11.jpg",
+  ];
 
-
-  ]
   return (
-    <>
-      <div className="w-full min-h-[80svh]">
-  <Swiper
-    spaceBetween={30}
-    effect="fade"
-    pagination={{ clickable: true }}
-    autoplay={{
-      delay: 3000,
-      disableOnInteraction: false,
-    }}
-    modules={[EffectFade, Pagination, Autoplay]}
-    className="w-full h-full"
-  >
-    {imgArr.map((img, index) => (
-      <SwiperSlide key={index} className="h-full">
-        <img
-          src={img}
-          className="w-full h-full object-cover md:object-cover"
-          alt="slide"
-        />
-      </SwiperSlide>
-    ))}
-  </Swiper>
-</div>
+    <div className="w-full 
+      h-[55svh] sm:h-[70svh] lg:h-[80svh]">
+      
+      <Swiper
+        spaceBetween={30}
+        effect="fade"
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[EffectFade, Pagination, Autoplay]}
+        className="w-full h-full"
+      >
+        {imgArr.map((img, index) => (
+          <SwiperSlide key={index} className="w-full h-full">
+            <img
+              src={img}
+              alt="slide"
+              className="w-full h-full object-cover object-center"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default Slider
+export default Slider;
